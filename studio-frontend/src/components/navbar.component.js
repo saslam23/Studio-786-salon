3d88c4ac5e85990a9955e9742309e04e1b622c1d
saslam23 <saadaslam13@gmail.com>
2022-09-25 12:55:34 -0500

    added links to other appt buttons

--- studio-frontend/src/components/navbar.component.js
+++ studio-frontend/src/components/navbar.component.js
@@ -9,7 +9,7 @@ export default function Navigation() {
     <Navbar fixed="top" bg="dark" expand="lg" variant="dark">
       <div className="navHeader">
       <div><Link  to="/" ><img className="studiologo" src="/assets/s786-white.png" alt="studio_786_logo"/></Link></div>
-      <button className="appointmentButton" >Book an Appointment</button>
+      <a target="_blank" style={{outline:'none', textDecoration:'none', color:'black !important'}} href="https://calendly.com/studio786salon"> <button className="appointmentButton" >Book an Appointment</button> </a>
       </div>
 
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -19,7 +19,7 @@ export default function Navigation() {
        <Link className="navi-item" to="/services" >Services</Link>
        <Link className="navi-item" to="/about"  >About</Link>
         <Link className="navi-item" to="/contact" >Contact</Link>
-        <button style={{outline:'none',minWidth:'210px', maxWidth:'210px', height:'40px', borderRadius:'5px', transform:'translateY(10px)',marginBottom:'10px'}}>Book an Appointment</button>
+        <a target="_blank" style={{outline:'none', textDecoration:'none', color:'black'}} href="https://calendly.com/studio786salon"><button style={{outline:'none',minWidth:'210px', maxWidth:'210px', height:'40px', borderRadius:'5px', transform:'translateY(10px)',marginBottom:'10px'}}>Book an Appointment</button></a>
       </Nav>
       </Navbar.Collapse>
     </Navbar> 
