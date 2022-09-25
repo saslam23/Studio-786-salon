16a472237d4b70ac8c5597b76abd23cc33e3e7b3
saslam23 <saadaslam13@gmail.com>
2022-09-25 11:34:25 -0500

    added booking appointment

--- studio-frontend/src/components/home.component.js
+++ studio-frontend/src/components/home.component.js
@@ -7,8 +7,11 @@ export default function Home() {
       <img className="banner-image" src="/assets/eyebrow-threading-2.jpg" alt="banner"/>  
       <div className="banner-info">
       <p className="banner-info-heading">Studio 786 Salon</p>
-      Studio 786 Salon is a premier Beauty Salon in the Wheaton area. Since 2016, we’ve offered a wide range of services and products 
-              to give you the elegant and fabulous look that you deserve. Scroll on to book an appointment or enjoy our walk-in services.
+      <p>Studio 786 Salon is a premier Beauty Salon in the Wheaton area. Since 2016, we’ve offered a wide range of services and products 
+              to give you the elegant and fabulous look that you deserve. Scroll on to book an appointment or enjoy our walk-in services.</p>
+              <a target="_blank" style={{outline:'none', textDecoration:'none', color:'black', fontSize:'20px'}} href="https://calendly.com/studio786salon"> <button style={{outline:'none',minWidth:'270px', maxWidth:'270px', height:'60px', borderRadius:'5px'}}>
+        Book an Appointment
+        </button></a>
       </div>
       </div>
       </>
