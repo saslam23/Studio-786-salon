c13d6c5fd321fb0645cd701f9bd04063214fa273
saslam23 <saadaslam13@gmail.com>
2022-09-25 12:47:53 -0500

    style changes and added appointment button to top

--- studio-frontend/src/components/navbar.component.js
+++ studio-frontend/src/components/navbar.component.js
@@ -7,7 +7,11 @@ export default function Navigation() {
   return (
     <div>
     <Navbar fixed="top" bg="dark" expand="lg" variant="dark">
-    <Link  to="/" ><img className="studiologo" src="/assets/s786-white.png" alt="studio_786_logo"/></Link>
+      <div className="navHeader">
+      <div><Link  to="/" ><img className="studiologo" src="/assets/s786-white.png" alt="studio_786_logo"/></Link></div>
+      <button className="appointmentButton" >Book an Appointment</button>
+      </div>
+
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
@@ -15,6 +19,7 @@ export default function Navigation() {
        <Link className="navi-item" to="/services" >Services</Link>
        <Link className="navi-item" to="/about"  >About</Link>
         <Link className="navi-item" to="/contact" >Contact</Link>
+        <button style={{outline:'none',minWidth:'210px', maxWidth:'210px', height:'40px', borderRadius:'5px', transform:'translateY(10px)',marginBottom:'10px'}}>Book an Appointment</button>
       </Nav>
       </Navbar.Collapse>
     </Navbar> 
